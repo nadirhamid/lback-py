cf0558e9ae2b45499c1ab022eb4d31df52742a9e
nadirhamid <matrix.nad@gmail.com>
2016-12-29 19:50:42 +0000

    Update source and make changes

--- lback/oneline-py/modules/Example_module.py
+++
@@ -1,8 +0,0 @@
-from oneline import ol
-class Example_module(ol.module):
-  def start(self):
-    self.pipeline = ol.stream()
-  def receiver(self,message):
-    self.pipeline.run(message)
-  def stop(self):
-    return None
